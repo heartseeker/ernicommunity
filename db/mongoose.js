c4d198d5c1b8d230ebec7a25c034fd41be5ef544
Amistad Alex (ERNI) <amal@erni.ph>
2018-08-08 08:54:13 +0800

    update

--- db/mongoose.js
+++ db/mongoose.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // connect to mongoose
 // ==============================================
-mongoose.connect('mongodb://localhost/ernicommunity')
-// mongoose.connect('mongodb://vouchadmin:dbvouch123@ds221228.mlab.com:21228/vouchme')
+// mongoose.connect('mongodb://localhost/ernicommunity')
+mongoose.connect('mongodb://erni:erni123456@ds115762.mlab.com:15762/ernicommunity')
 .then(() => {
     console.log('Connect to mongodb success!');
 })
